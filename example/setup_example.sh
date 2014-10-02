c6b5db31dad29b4845b18dc4a5c37ee940342652
Matthew Pearson <pearsonmr@ornl.gov>
2014-10-02 10:41:49 -0700

    ADnED: use two detectors. Add back in NDArray pool infomation polling.

--- example/setup_example.sh
+++ example/setup_example.sh
@@ -3,11 +3,11 @@
 caput -c -S BL99:Det:ADnED:PVName "neutrons"
 caput -c BL99:Det:ADnED:EventDebug 0
 caput -c BL99:Det:ADnED:Det1:PixelNumStart 0
-caput -c BL99:Det:ADnED:Det1:PixelNumEnd 200
-caput -c BL99:Det:ADnED:Det2:PixelNumStart 2100
-caput -c BL99:Det:ADnED:Det2:PixelNumEnd 2400
+caput -c BL99:Det:ADnED:Det1:PixelNumEnd 1023
+caput -c BL99:Det:ADnED:Det2:PixelNumStart 2048
+caput -c BL99:Det:ADnED:Det2:PixelNumEnd 3072
 caput -c BL99:Det:ADnED:TOFMax 160000
-caput -c BL99:Det:ADnED:NumDetectors 3
-caput -c BL99:Det:ADnED:Det3:PixelNumStart 2700
-caput -c BL99:Det:ADnED:Det3:PixelNumEnd 2800
+caput -c BL99:Det:ADnED:NumDetectors 2
+caput -c BL99:Det:ADnED:Det3:PixelNumStart 0
+caput -c BL99:Det:ADnED:Det3:PixelNumEnd 0
 
