c84450a2c56dbb5518a868d33d204f2de7dcacee
Matthew Pearson <pearsonmr@ornl.gov>
2014-10-02 12:58:57 -0700

    ADnED: add in ROI and StdArray plugins to example IOC to extract the TOF spectra.

--- example/iocBoot/iocexample/st.cmd
+++ example/iocBoot/iocexample/st.cmd
@@ -18,6 +18,26 @@ ADnEDConfig("NED", -1, -1, 1)
 
 #asynSetTraceMask("NED",0,0x11)
 
+#ROI plugins to extract TOF data
+NDROIConfigure("DET1.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+NDROIConfigure("DET2.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+NDROIConfigure("DET3.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+NDROIConfigure("DET4.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+NDROIConfigure("DET5.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+NDROIConfigure("DET6.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+NDROIConfigure("DET7.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+NDROIConfigure("DET8.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+
+#Standard arrays plugin for channel access visualisation of the TOF arrays
+NDStdArraysConfigure("DET1.TOF.ARR", 100, 0, "DET1.TOF", 0, -1, 0, 0)
+NDStdArraysConfigure("DET2.TOF.ARR", 100, 0, "DET2.TOF", 0, -1, 0, 0)
+NDStdArraysConfigure("DET3.TOF.ARR", 100, 0, "DET3.TOF", 0, -1, 0, 0)
+NDStdArraysConfigure("DET4.TOF.ARR", 100, 0, "DET4.TOF", 0, -1, 0, 0)
+NDStdArraysConfigure("DET5.TOF.ARR", 100, 0, "DET5.TOF", 0, -1, 0, 0)
+NDStdArraysConfigure("DET6.TOF.ARR", 100, 0, "DET6.TOF", 0, -1, 0, 0)
+NDStdArraysConfigure("DET7.TOF.ARR", 100, 0, "DET7.TOF", 0, -1, 0, 0)
+NDStdArraysConfigure("DET8.TOF.ARR", 100, 0, "DET8.TOF", 0, -1, 0, 0)
+
 #####################################################
 
 ## Load record instances
