8b9398ece1346c66d34a51582e13889a683c27e0
Matthew Pearson <pearsonmr@ornl.gov>
2014-10-03 08:21:33 -0700

    ADnED: configure plugins too. This all needs to be done via PVTable?

--- example/setup_example.sh
+++ example/setup_example.sh
@@ -10,4 +10,14 @@ caput -c BL99:Det:ADnED:TOFMax 160000
 caput -c BL99:Det:ADnED:NumDetectors 2
 caput -c BL99:Det:ADnED:Det3:PixelNumStart 0
 caput -c BL99:Det:ADnED:Det3:PixelNumEnd 0
+caput -c BL99:Det:ADnED:ArrayCallbacks 1
 
+caput -c BL99:Det:ADnED:Det1:TOF:EnableCallbacks 1
+caput -c BL99:Det:ADnED:Det1:TOF:MinX 2049
+caput -c BL99:Det:ADnED:Det1:TOF:SizeX 160000
+caput -c BL99:Det:ADnED:Det1:TOF:Array:EnableCallbacks 1
+
+caput -c BL99:Det:ADnED:Det1:TOF:EnableCallbacks 1
+caput -c BL99:Det:ADnED:Det1:TOF:MinX 162050
+caput -c BL99:Det:ADnED:Det1:TOF:SizeX 160000
+caput -c BL99:Det:ADnED:Det1:TOF:Array:EnableCallbacks 1
