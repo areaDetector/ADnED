74e93861d51b0bb3de2104a0ffd0af2388adef0e
Matthew Pearson <pearsonmr@ornl.gov>
2014-10-03 15:37:33 -0700

    ADnED: remove detectors 5-8. Add in new ROIs and Stats plugins to do ROI on TOF spectrums.

--- example/iocBoot/iocexample/st.cmd
+++ example/iocBoot/iocexample/st.cmd
@@ -23,20 +23,24 @@ NDROIConfigure("DET1.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
 NDROIConfigure("DET2.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
 NDROIConfigure("DET3.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
 NDROIConfigure("DET4.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
-NDROIConfigure("DET5.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
-NDROIConfigure("DET6.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
-NDROIConfigure("DET7.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
-NDROIConfigure("DET8.TOF", 100, 0, "NED", 0, -1, -1, 0, 0)
+
+#ROI plugins to do ROI on the TOF spectrums
+NDROIConfigure("DET1.TOF.ROI1", 100, 0, "DET1.TOF", 0, -1, -1, 0, 0)
+NDROIConfigure("DET2.TOF.ROI1", 100, 0, "DET2.TOF", 0, -1, -1, 0, 0)
+NDROIConfigure("DET3.TOF.ROI1", 100, 0, "DET3.TOF", 0, -1, -1, 0, 0)
+NDROIConfigure("DET4.TOF.ROI1", 100, 0, "DET4.TOF", 0, -1, -1, 0, 0)
 
 #Standard arrays plugin for channel access visualisation of the TOF arrays
 NDStdArraysConfigure("DET1.TOF.ARR", 100, 0, "DET1.TOF", 0, -1, 0, 0)
 NDStdArraysConfigure("DET2.TOF.ARR", 100, 0, "DET2.TOF", 0, -1, 0, 0)
 NDStdArraysConfigure("DET3.TOF.ARR", 100, 0, "DET3.TOF", 0, -1, 0, 0)
 NDStdArraysConfigure("DET4.TOF.ARR", 100, 0, "DET4.TOF", 0, -1, 0, 0)
-NDStdArraysConfigure("DET5.TOF.ARR", 100, 0, "DET5.TOF", 0, -1, 0, 0)
-NDStdArraysConfigure("DET6.TOF.ARR", 100, 0, "DET6.TOF", 0, -1, 0, 0)
-NDStdArraysConfigure("DET7.TOF.ARR", 100, 0, "DET7.TOF", 0, -1, 0, 0)
-NDStdArraysConfigure("DET8.TOF.ARR", 100, 0, "DET8.TOF", 0, -1, 0, 0)
+
+#Stats plugins for ROI on TOF spectra
+NDStatsConfigure("DET1.TOF.STAT1", 100, 0, "DET1.TOF.ROI1", 0, -1, -1, 0, 0)
+NDStatsConfigure("DET2.TOF.STAT1", 100, 0, "DET2.TOF.ROI1", 0, -1, -1, 0, 0)
+NDStatsConfigure("DET3.TOF.STAT1", 100, 0, "DET3.TOF.ROI1", 0, -1, -1, 0, 0)
+NDStatsConfigure("DET4.TOF.STAT1", 100, 0, "DET4.TOF.ROI1", 0, -1, -1, 0, 0)
 
 #####################################################
 
--- example/setup_example.sh
+++ example/setup_example.sh
@@ -6,21 +6,35 @@ caput -c BL99:Det:ADnED:TOFMax 160000
 caput -c BL99:Det:ADnED:NumDetectors 2
 caput -c BL99:Det:ADnED:ArrayCallbacks 1
 
+echo "Det1"
+
 caput -c BL99:Det:ADnED:Det1:Description "Det 1"
 caput -c BL99:Det:ADnED:Det1:PixelNumStart 0
 caput -c BL99:Det:ADnED:Det1:PixelNumEnd 1023
+
 caput -c BL99:Det:ADnED:Det1:TOF:EnableCallbacks 1
 caput -c BL99:Det:ADnED:Det1:TOF:MinX 2049
 caput -c BL99:Det:ADnED:Det1:TOF:SizeX 160000
 caput -c BL99:Det:ADnED:Det1:TOF:Array:EnableCallbacks 1
 
+caput -c BL99:Det:ADnED:Det1:TOF:ROI1:EnableCallbacks 1
+caput -c BL99:Det:ADnED:Det1:TOF:Stat1:EnableCallbacks 1
+caput -c BL99:Det:ADnED:Det1:TOF:Stat1:BgdWidth 0
+
+echo "Det2"
+
 caput -c BL99:Det:ADnED:Det2:Description "Det 2"
 caput -c BL99:Det:ADnED:Det2:PixelNumStart 2048
 caput -c BL99:Det:ADnED:Det2:PixelNumEnd 3072
+
 caput -c BL99:Det:ADnED:Det2:TOF:EnableCallbacks 1
 caput -c BL99:Det:ADnED:Det2:TOF:MinX 162050
 caput -c BL99:Det:ADnED:Det2:TOF:SizeX 160000
 caput -c BL99:Det:ADnED:Det2:TOF:Array:EnableCallbacks 1
 
-caput -c BL99:Det:ADnED:Det3:PixelNumStart 0
-caput -c BL99:Det:ADnED:Det3:PixelNumEnd 0
+caput -c BL99:Det:ADnED:Det2:TOF:ROI1:EnableCallbacks 1
+caput -c BL99:Det:ADnED:Det2:TOF:Stat1:EnableCallbacks 1
+caput -c BL99:Det:ADnED:Det2:TOF:Stat1:BgdWidth 0
+
+echo "Done"
+
