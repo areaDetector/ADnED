afff522b99bb9519058aca4620b37ae7c7bc3030
Matthew Pearson <pearsonmr@ornl.gov>
2014-10-03 10:40:06 -0700

    ADnED: add description record for each detector.

--- example/setup_example.sh
+++ example/setup_example.sh
@@ -2,22 +2,25 @@
 
 caput -c -S BL99:Det:ADnED:PVName "neutrons"
 caput -c BL99:Det:ADnED:EventDebug 0
-caput -c BL99:Det:ADnED:Det1:PixelNumStart 0
-caput -c BL99:Det:ADnED:Det1:PixelNumEnd 1023
-caput -c BL99:Det:ADnED:Det2:PixelNumStart 2048
-caput -c BL99:Det:ADnED:Det2:PixelNumEnd 3072
 caput -c BL99:Det:ADnED:TOFMax 160000
 caput -c BL99:Det:ADnED:NumDetectors 2
-caput -c BL99:Det:ADnED:Det3:PixelNumStart 0
-caput -c BL99:Det:ADnED:Det3:PixelNumEnd 0
 caput -c BL99:Det:ADnED:ArrayCallbacks 1
 
+caput -c BL99:Det:ADnED:Det1:Description "Det 1"
+caput -c BL99:Det:ADnED:Det1:PixelNumStart 0
+caput -c BL99:Det:ADnED:Det1:PixelNumEnd 1023
 caput -c BL99:Det:ADnED:Det1:TOF:EnableCallbacks 1
 caput -c BL99:Det:ADnED:Det1:TOF:MinX 2049
 caput -c BL99:Det:ADnED:Det1:TOF:SizeX 160000
 caput -c BL99:Det:ADnED:Det1:TOF:Array:EnableCallbacks 1
 
-caput -c BL99:Det:ADnED:Det1:TOF:EnableCallbacks 1
-caput -c BL99:Det:ADnED:Det1:TOF:MinX 162050
-caput -c BL99:Det:ADnED:Det1:TOF:SizeX 160000
-caput -c BL99:Det:ADnED:Det1:TOF:Array:EnableCallbacks 1
+caput -c BL99:Det:ADnED:Det2:Description "Det 2"
+caput -c BL99:Det:ADnED:Det2:PixelNumStart 2048
+caput -c BL99:Det:ADnED:Det2:PixelNumEnd 3072
+caput -c BL99:Det:ADnED:Det2:TOF:EnableCallbacks 1
+caput -c BL99:Det:ADnED:Det2:TOF:MinX 162050
+caput -c BL99:Det:ADnED:Det2:TOF:SizeX 160000
+caput -c BL99:Det:ADnED:Det2:TOF:Array:EnableCallbacks 1
+
+caput -c BL99:Det:ADnED:Det3:PixelNumStart 0
+caput -c BL99:Det:ADnED:Det3:PixelNumEnd 0
