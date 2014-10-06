ec70c83e7247d95a9906ab293f12f3d1428649d3
Matthew Pearson <pearsonmr@ornl.gov>
2014-10-06 14:41:57 -0700

    ADnED: add in autosave support.

--- example/iocBoot/iocexample/st.cmd
+++ example/iocBoot/iocexample/st.cmd
@@ -7,6 +7,8 @@
 
 cd ${TOP}
 
+epicsEnvSet("ADNED", "BL99:Det:ADnED")
+
 ## Register all support components
 dbLoadDatabase "dbd/example.dbd"
 example_registerRecordDeviceDriver pdbbase
@@ -44,10 +46,37 @@ NDStatsConfigure("DET4.TOF.STAT1", 100, 0, "DET4.TOF.ROI1", 0, -1, -1, 0, 0)
 
 #####################################################
 
+#################################################
+# autosave
+
+epicsEnvSet IOCNAME example
+epicsEnvSet SAVE_DIR /home/controls/var/ADnED
+
+save_restoreSet_Debug(0)
+
+### status-PV prefix, so save_restore can find its status PV's.
+save_restoreSet_status_prefix("BL99:CS:Det:ADnED")
+
+set_requestfile_path("$(SAVE_DIR)")
+set_savefile_path("$(SAVE_DIR)")
+
+save_restoreSet_NumSeqFiles(3)
+save_restoreSet_SeqPeriodInSeconds(600)
+set_pass0_restoreFile("$(IOCNAME).sav")
+set_pass1_restoreFile("$(IOCNAME).sav")
+
+#################################################
+
 ## Load record instances
 dbLoadRecords "db/example.db"
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
+
+# Create request file and start periodic 'save'
+epicsThreadSleep(5)
+create_monitor_set("$(IOCNAME).req", 5)
+
+
 
 
