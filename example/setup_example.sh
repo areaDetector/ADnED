ac3083239bd22edeb3b7ab2625097c6bcb173928
Matthew Pearson <pearsonmr@ornl.gov>
2014-10-07 13:14:26 -0700

    ADnED: add Pixel X/Y ROI template file. Add use of the ADnEDPixelROI into the example IOC. Add into .req file template.

--- example/setup_example.sh
+++ example/setup_example.sh
@@ -6,35 +6,50 @@ caput -c BL99:Det:ADnED:TOFMax 160000
 caput -c BL99:Det:ADnED:NumDetectors 2
 caput -c BL99:Det:ADnED:ArrayCallbacks 1
 
-echo "Det1"
+echo "Det1..."
 
 caput -c BL99:Det:ADnED:Det1:Description "Det 1"
 caput -c BL99:Det:ADnED:Det1:PixelNumStart 0
 caput -c BL99:Det:ADnED:Det1:PixelNumEnd 1023
 
 caput -c BL99:Det:ADnED:Det1:TOF:EnableCallbacks 1
-caput -c BL99:Det:ADnED:Det1:TOF:MinX 2049
-caput -c BL99:Det:ADnED:Det1:TOF:SizeX 160000
 caput -c BL99:Det:ADnED:Det1:TOF:Array:EnableCallbacks 1
 
 caput -c BL99:Det:ADnED:Det1:TOF:ROI1:EnableCallbacks 1
 caput -c BL99:Det:ADnED:Det1:TOF:Stat1:EnableCallbacks 1
 caput -c BL99:Det:ADnED:Det1:TOF:Stat1:BgdWidth 0
 
-echo "Det2"
+caput -c BL99:Det:ADnED:Det1:XY:EnableCallbacks 1
+
+echo "Det2..."
 
 caput -c BL99:Det:ADnED:Det2:Description "Det 2"
 caput -c BL99:Det:ADnED:Det2:PixelNumStart 2048
 caput -c BL99:Det:ADnED:Det2:PixelNumEnd 3072
 
 caput -c BL99:Det:ADnED:Det2:TOF:EnableCallbacks 1
-caput -c BL99:Det:ADnED:Det2:TOF:MinX 162050
-caput -c BL99:Det:ADnED:Det2:TOF:SizeX 160000
 caput -c BL99:Det:ADnED:Det2:TOF:Array:EnableCallbacks 1
 
 caput -c BL99:Det:ADnED:Det2:TOF:ROI1:EnableCallbacks 1
 caput -c BL99:Det:ADnED:Det2:TOF:Stat1:EnableCallbacks 1
 caput -c BL99:Det:ADnED:Det2:TOF:Stat1:BgdWidth 0
+ 
+caput -c BL99:Det:ADnED:Det2:XY:EnableCallbacks 1
+
+echo "Allocate Space..."
+caput -c BL99:Det:ADnED:AllocSpace.PROC 1
+
+echo "Auto config ROI sizes..."
+caput -c BL99:Det:ADnED:Det1:ConfigTOFStart.PROC 1
+caput -c BL99:Det:ADnED:Det2:ConfigTOFStart.PROC 1
+caput -c BL99:Det:ADnED:Det3:ConfigTOFStart.PROC 1
+caput -c BL99:Det:ADnED:Det4:ConfigTOFStart.PROC 1
+
+caput -c BL99:Det:ADnED:Det1:ConfigXYStart.PROC 1
+caput -c BL99:Det:ADnED:Det2:ConfigXYStart.PROC 1
+caput -c BL99:Det:ADnED:Det3:ConfigXYStart.PROC 1
+caput -c BL99:Det:ADnED:Det4:ConfigXYStart.PROC 1
+
 
 echo "Done"
 
